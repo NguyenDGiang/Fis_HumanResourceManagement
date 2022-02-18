8520f162726f7b02e91f3727a4c77d764e1f7335
tkangf <88397082+tkangf@users.noreply.github.com>
2022-02-18 16:23:05 +0700

    controller/nhom_1

---
+++ HRM/HRM/Rpc/village/Village_DistrictDTO.cs
@@ -0,0 +1,51 @@
+﻿using TrueSight.Common;
+using HRM.Entities;
+
+
+namespace HRM.Rpc.village
+{
+    public class Village_DistrictDTO : DataDTO
+    {
+
+
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public long ProvinceId { get; set; }
+
+        public long StatusId { get; set; }
+
+
+        public Village_DistrictDTO() { }
+        public Village_DistrictDTO(District District)
+        {
+
+            this.Id = District.Id;
+
+            this.Name = District.Name;
+
+
+            this.ProvinceId =(long) District.ProvinceId;
+
+            this.StatusId = (long)District.StatusId;
+
+        }
+    }
+
+    public class Village_DistrictFilterDTO : FilterDTO
+    {
+
+        public IdFilter Id { get; set; }
+
+        public StringFilter Name { get; set; }
+
+        public IdFilter ProvinceId { get; set; }
+
+        public IdFilter StatusId { get; set; }
+
+        public DistrictOrder OrderBy { get; set; }
+    }
+
+}
---
+++ HRM/HRM/Rpc/village/Village_VillageDTO.cs
@@ -0,0 +1,42 @@
+﻿using TrueSight.Common;
+using HRM.Entities;
+
+namespace HRM.Rpc.village
+{
+    public class Village_VillageDTO : DataDTO
+    {
+
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public long DistrictId { get; set; }
+        public long StatusId { get; set; }
+        public bool Used { get; set; }
+        public Village_DistrictDTO District { get; set; }
+        public Village_StatusDTO Status { get; set; }
+        public Village_VillageDTO() { }
+        public Village_VillageDTO(Village Village)
+        {
+            this.Id = Village.Id;
+            this.Code = Village.Code;
+            this.Name = Village.Name;
+            this.DistrictId = (long)Village.DistrictId;
+            this.StatusId = (long)Village.StatusId;
+            this.Used = Village.Used;
+            this.District = Village.District == null ? null : new Village_DistrictDTO(Village.District);
+            this.Status = Village.Status == null ? null : new Village_StatusDTO(Village.Status);
+            this.Errors = Village.Errors;
+        }
+    }
+
+    public class Village_VillageFilterDTO : FilterDTO
+    {
+        public IdFilter Id { get; set; }
+        public StringFilter Code { get; set; }
+        public StringFilter Name { get; set; }
+        public IdFilter DistrictId { get; set; }
+        public IdFilter StatusId { get; set; }
+        public VillageOrder OrderBy { get; set; }
+    }
+
+}
